1935e27d6996b15622fbf20a03bcef336a3c220e
aneeshkichu98 <94885463+aneeshs98@users.noreply.github.com>
2021-11-27 17:46:46 +0530

    jwt token added

--- AssetManagementAPI/WebApplication1/Startup.cs
+++ AssetManagementAPI/WebApplication1/Startup.cs
@@ -12,6 +12,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace WebApplication1
 {
@@ -30,6 +33,19 @@ namespace WebApplication1
             var connection = Configuration.GetConnectionString("AssetDBDatabase");
             services.AddDbContextPool<AssetDBContext>(options => options.UseSqlServer(connection));
             services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +59,8 @@ namespace WebApplication1
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
